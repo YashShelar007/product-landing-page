e1ab586e229f8e6fc63384caf8697b04cebd8525
YashShelar007 <yshelar@asu.edu>
2021-07-30 02:41:00 -0700

    code clean up for packages, customers

--- shared.js
+++ shared.js
@@ -16,16 +16,20 @@ for(let i = 0; i < planButtons.length; i++) {
 
 backdrop.addEventListener("click", function() {
     //mobileNav.style.display = "none";
-    mobileNav.classList.add("open");
+    mobileNav.classList.remove("open");
     closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
+if(modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal() {
     //backdrop.style.display = "none";
     //modal.style.display = "none";
-    modal.classList.remove("open");
+    if (modalNoButton) {
+        modal.classList.remove("open");
+    }
     backdrop.classList.remove("open");
 }
 
